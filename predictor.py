8b6444974d9392990e5669939c7bf4c8b75c5370
Justin Tomlinson <justintomlinsonn@gmail.com>
2020-11-06 11:25:15 -0500

    Added remaining ballots
    Justin Tomlinson
    Nov 6, 2020

--- predictor.py
+++ predictor.py
@@ -24,12 +24,12 @@ for race in data['data']['races']:
       except:
          trump_percentage = 0
          biden_percentage = 0
-      county_trump_expected = (math.floor((((100 - county_returned) / 100.0)
-                         * trump_percentage * county_trump_votes))
-                         + county_trump_votes)
-      county_biden_expected = (math.floor((((100 - county_returned) / 100.0)
-                         * biden_percentage * county_biden_votes))
-                         + county_biden_votes)
+      county_trump_remaining = (math.floor((((100 - county_returned) / 100.0)
+                               * trump_percentage * county_trump_votes)))
+      county_biden_remaining = (math.floor((((100 - county_returned) / 100.0)
+                               * biden_percentage * county_biden_votes)))
+      county_trump_expected = county_trump_remaining + county_trump_votes
+      county_biden_expected = county_biden_remaining + county_biden_votes
 
       state_trump_votes += county_trump_votes
       state_biden_votes += county_biden_votes
@@ -43,9 +43,10 @@ for race in data['data']['races']:
       biden_electoral_votes += race['electoral_votes']
       print('{:<20s} {:<10s}'.format(race['state_name'], 'D +' + str(race['electoral_votes'])),end='')
    gap = abs(state_trump_expected - state_biden_expected)
-   print(' Gap: ' + str(gap), end='')
+   print(' Expected Gap: ' + str(gap), end='')
    percent_gap = (gap * 100.0) / (state_trump_expected + state_biden_expected)
-   print('\t(' + format(percent_gap,'.2f') + '%)')
+   print('{:<12s} {:<10s}'.format('\t(' + format(percent_gap,'.2f') + '%)'
+         ,'Remaining: ' + str((state_trump_expected + state_biden_expected) - (state_trump_votes + state_biden_votes))))
 
 print('\nTrump electoral votes: ' + str(trump_electoral_votes))
 print('Biden electoral votes: ' + str(biden_electoral_votes))
